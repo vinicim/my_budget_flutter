c4b74da03a8f4514b19e34ccf0718403f75b7944
Vinícius Vieira Meneses <vinicius.meneses@tst.jus.br>
2022-04-30 23:01:51 -0300

    criação de dois widgets(stateless) principais; no segundo widget de transações foi definida sua estrutura

---
+++ lib/model/transaction.dart
@@ -0,0 +1,12 @@
+class Transaction {
+  final String id;
+  final double value;
+  final String title;
+  final DateTime date;
+
+  Transaction(
+      {required this.id,
+      required this.value,
+      required this.title,
+      required this.date});
+}
